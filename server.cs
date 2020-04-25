718da00075f197747f4e334b639264b8b54f518e
Electrk <Electrk@ymail.com>
2020-04-24 17:09:35 -0700

    Added testcoaster.cs to .gitignore

--- server.cs
+++ server.cs
@@ -1,6 +1,4 @@
 exec ("./lib/Support_Shapelines/server.cs");
-exec ("./lib/Support_DirectedGridGraph.cs");
-exec ("./3DGridGraph.cs");
 exec ("./createDestroy.cs");
 exec ("./nodes.cs");
 exec ("./cameras.cs");
