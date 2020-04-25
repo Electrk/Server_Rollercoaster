8af5058070e2c8cebd7dded57f44b56c79b7a8ed
Electrk <Electrk@ymail.com>
2020-04-25 00:38:23 -0700

    Removed pointless RollercoasterCamera ScriptObject

--- server.cs
+++ server.cs
@@ -1,7 +1,7 @@
 exec ("./lib/Support_Shapelines/server.cs");
 exec ("./createDestroy.cs");
 exec ("./nodes.cs");
-exec ("./cameras.cs");
+exec ("./camera.cs");
 exec ("./riders.cs");
 exec ("./utility.cs");
 exec ("./debug.cs");
